cb8df520d3d9194ef1e40bb623bf6a705ea0dcbd
Li Lin <guru.lin@gmail.com>
2019-10-09 21:29:48 +0800

    removed prometheus listener

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,13 +3,9 @@ package main
 import (
 	"context"
 	"grpc-account-svc/app/server"
-	"net/http"
 	"os"
 
-	"github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/izumin5210/grapi/pkg/grapiserver"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-
 	"google.golang.org/grpc/grpclog"
 )
 
@@ -21,32 +17,12 @@ func main() {
 	}
 }
 
-func prometheusMiddleWare(next http.Handler) http.Handler {
-	handler := promhttp.Handler()
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/metrics" {
-			grpclog.Info("/metrics")
-			handler.ServeHTTP(w, r)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func run() error {
-	// Application context
 	ctx := context.Background()
-
-	// Create some standard server metrics.
-	grpcMetrics := grpc_prometheus.NewServerMetrics()
-
 	s := grapiserver.New(
 		grapiserver.WithDefaultLogger(),
 		grapiserver.WithGrpcAddr("tcp", ":3101"),
 		grapiserver.WithGatewayAddr("tcp", ":3100"),
-		grapiserver.WithGatewayServerMiddlewares(prometheusMiddleWare),
-		grapiserver.WithGrpcServerStreamInterceptors(grpcMetrics.StreamServerInterceptor()),
-		grapiserver.WithGrpcServerUnaryInterceptors(grpcMetrics.UnaryServerInterceptor()),
 		grapiserver.WithServers(
 			server.NewAccountServiceServer(),
 		),
